4a50b28476ae7ad150b5aa9dc88b0905aabeed31
chaos7x <85663230+chaos7x@users.noreply.github.com>
2026-09-18 15:34:42 +0200

    Update get_token.py
    Header entfernt

--- src/get_token/get_token.py
+++ src/get_token/get_token.py
@@ -3,17 +3,6 @@
 # Ausführung (./get_token.py); das eigentliche Ausführbar-Bit übersteht
 # Git-Checkouts/Web-Uploads nicht zuverlässig, das Skript läuft ganz normal
 # auch via `python3 get_token.py` ohne +x.
-# Copyright (C) 2026 Chaos7x
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
 
 import json
 import os
